624e5601cbc928bcd3ee3a4a6c72ee8821acb3d5
franckngakou <franckngakou@gmail.com>
2022-05-28 15:41:50 +0100

    fisrt commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    value= '0';
+    oldvalue= '0';
+    lastoperator='';
+    readyForNewInput= true;
+    numberGroups=[[7,8,9,'c'],
+    [4,5,6,'-'],
+    [1,2,3,'+'],
+    [0,'x','.','/']];
+  onButtonPress(num){
+    console.log(num)
+    if ( typeof num =='number'|| num=='.') {
+       console.log('is a number')
+       if ( this.readyForNewInput)
+         this.value=''+ num;
+        else
+          this.value += '' + num;
+          this.readyForNewInput=false;
 
-  constructor() {}
+    }
+    else if(num=='c'){
+       this.value='0';
+       this.readyForNewInput= true;
+    }
+    else if(num=='='){
+       if(this.lastoperator=='x')
+       this.value=''+(parseFloat(this.oldvalue)*parseFloat(this.value));
+       if(this.lastoperator=='-')
+       this.value=''+(parseFloat(this.oldvalue)-parseFloat(this.value));
+       if(this.lastoperator=='+')
+       this.value=''+(parseFloat(this.oldvalue)+parseFloat(this.value));
+       if(this.lastoperator=='/')
+       this.value=''+(parseFloat(this.oldvalue)/parseFloat(this.value));
+       this.readyForNewInput= true;
+    }
+    else{///operator
+      this.readyForNewInput= true;
+      this.oldvalue= this.value;
+      this.lastoperator=num;
+    }
+  }
 
 }
