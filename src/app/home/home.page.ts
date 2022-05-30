f5020fc4619f1ca2795388ef01fed973e9f92089
Donald Feuzing <ntemmado@gmail.com>
2022-05-30 12:06:07 +0200

    tache donald Feuzing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,55 +7,55 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-    value= '0';
-    oldvalue= '0';
-    lastoperator='';
-    virgule=false;
-    readyForNewInput= true;
-    numberGroups=[[7,8,9,'c'],
-    [4,5,6,'-'],
-    [1,2,3,'+'],
-    [0,'x','.','/']];
+  value = '0';
+  oldvalue = '0';
+  lastoperator = '';
+  virgule = false;
+  readyForNewInput = true;
+  numberGroups = [[7, 8, 9, 'c'],
+  [4, 5, 6, '-'],
+  [1, 2, 3, '+'],
+  [0, 'x', '.', '/']];
 
-  onButtonPress(num){
+  onButtonPress(num) {
     console.log(num)
-    if ( typeof num =='number') {
+    if (typeof num == 'number') {
 
-       if ( this.readyForNewInput)
-         this.value=''+ num;
-        else
-          this.value += '' + num;
-          this.readyForNewInput=false;
-
-    }
-    else if (num =='.') {
-      if(!this.virgule) this.value += '' + num;
-      this.virgule=true;
-      this.readyForNewInput=false;
+      if (this.readyForNewInput)
+        this.value = '' + num;
+      else
+        this.value += '' + num;
+      this.readyForNewInput = false;
 
     }
-    else if(num=='c'){
-       this.value='0';
-       this.readyForNewInput= true;
-       this.virgule=false
+    else if (num == '.') {
+      if (!this.virgule) this.value += '' + num;
+      this.virgule = true;
+      this.readyForNewInput = false;
+
     }
-    else if(num=='='){
-      this.virgule=false
-       if(this.lastoperator=='x')
-       this.value=''+(parseFloat(this.oldvalue)*parseFloat(this.value));
-       if(this.lastoperator=='-')
-       this.value=''+(parseFloat(this.oldvalue)-parseFloat(this.value));
-       if(this.lastoperator=='+')
-       this.value=''+(parseFloat(this.oldvalue)+parseFloat(this.value));
-       if(this.lastoperator=='/')
-       this.value=''+(parseFloat(this.oldvalue)/parseFloat(this.value));
-       this.readyForNewInput= true;
+    else if (num == 'c') {
+      this.value = '0';
+      this.readyForNewInput = true;
+      this.virgule = false
     }
-    else{///operator
-      this.readyForNewInput= true;
-      this.oldvalue= this.value;
-      this.lastoperator=num;
-      this.virgule=false
+    else if (num == '=') {
+      this.virgule = false
+      if (this.lastoperator == 'x')
+        this.value = '' + (parseFloat(this.oldvalue) * parseFloat(this.value));
+      if (this.lastoperator == '-')
+        this.value = '' + (parseFloat(this.oldvalue) - parseFloat(this.value));
+      if (this.lastoperator == '+')
+        this.value = '' + (parseFloat(this.oldvalue) + parseFloat(this.value));
+      if (this.lastoperator == '/')
+        this.value = '' + (parseFloat(this.oldvalue) / parseFloat(this.value));
+      this.readyForNewInput = true;
+    }
+    else {///operator
+      this.readyForNewInput = true;
+      this.oldvalue = this.value;
+      this.lastoperator = num;
+      this.virgule = false
     }
   }
 
